eb262db3d5e55727cfde7dc2e592148784b70d4e
xctangApp <99854995+xctangApp@users.noreply.github.com>
2023-07-21 21:16:28 -0500

    Create GPIOMusicBox.py

---
+++ Day05/GPIOMusicBox.py
@@ -0,0 +1,12 @@
+import pygame
+from gpiozero import Button
+
+pygame.init()
+
+button_sounds = {Button(4): pygame.mixer.Sound("/home/pi/gpio-music-box/samples/drum_tom_mid_hard.wav"),
+                 Button(17): pygame.mixer.Sound("/home/pi/gpio-music-box/samples/drum_cymbal_hard.wav"),
+                 Button(27): pygame.mixer.Sound("/home/pi/gpio-music-box/samples/drum_snare_hard.wav"),
+                 Button(10): pygame.mixer.Sound("/home/pi/gpio-music-box/samples/drum_cowbell.wav")}
+
+for button, sound in button_sounds.items():
+    button.when_pressed = sound.play
